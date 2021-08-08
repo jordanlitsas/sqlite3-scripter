adb986556cc57a5f66d3d13ed5fbc613d053d299
Jordan Litsas <jordanlitsas@live.com.au>
2021-08-08 22:32:51 +1000

    End to end config. User now can set user&db name

--- server.js
+++ server.js
@@ -2,47 +2,80 @@ let express = require("express");
 const { dirname } = require("path");
 let app = express();
 
-app.use(express.json());
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false}));
-
-//var app = require('express')();
 let http = require('http').createServer(app);
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://sit725-prac4:<prac4-pw>@cluster0.gwxoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
-const { MongoClient } = require('mongodb');
-const { nextTick } = require("process");
-const uri = "mongodb+srv://sit725-prac4:<prac4-pw>@cluster0.gwxoy.mongodb.net/db1?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
-
-try {
-  client.connect(err => {
-    const collection = client.db("db1").collection("users");
-    
-    collection.insertOne({id: 1})  // perform actions on the collection object
-    // client.close();
-  });
-}
-catch(err){
-}
-
 
 
 var port = process.env.PORT || 3000;
-
 app.use(express.static(__dirname + '/public'));
 
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://prac4-pw:prac4-pw@cluster0.gwxoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let userCollection;
+
+//Opens connection to MongoDB database.
+const openConnection = (message) => {
+  client.connect((err, db) => {
+    userCollection = client.db("newDB").collection('users');
+    if (!err){
+      console.log("DB Connected.")
+    }
+    else{
+      console.log(err)
+    }
+    
+  });
+}
+openConnection();
+
+const insertUser = (user, res) => {
+  userCollection.insert(user, (err, result) => {
+    console.log("User Inserted", result)
+    res.send({result: 200})
+  })
+}
 
 
 
-app.get("/sqlite3DatabaseProject", function (request, response) {
-  response.redirect(__dirname + "/public/dmbs.html")
+app.post('/createDatabase', function(req, res){
+
+  let user = req.body;
+  userCollection.find({'name': user.name}).toArray(function(err, result){
+    if (result.length == 1){
+      //handle user/db names taken
+    }
+    else if (result.length == 0){
+       insertUser(user, res);
+    }
+  })
+
 });
+
+app.post('/getDatabase', function(req, res){
+  let data = req.body;
+  let user = getUser(res, data);
+
+});
+
+
+const getUser = (res, data) => {
+  userCollection.find({'name': data.name}).toArray(function(err, result){
+    if (err) throw err;
+    res.send(result);
+  })
+}
+
+http.listen(port,()=>{
+  console.log("Listening on port ", port);
+});
+
+
 
 app.get("/mainProject", function (request, response) {
   var html = `This page will present my main project for this unit.`
@@ -57,31 +90,4 @@ app.get("/chessProject", function (request, response) {
 app.get("/weatherApp", function(request, response) {
   response.end("Im in the process of finding where I left those files ........");
 });
-app.post("/switch", function(request, response) {
-  var cb = request.body.cb;
 
-  if (cb === 'on'){
-  
-    
-
-  }
-  else {
-    response.end("Switch is off.");
-  }
-});
-
-
-
-
-
-
-
-app.post('/user-entry', function(request, response){
-
-});
-
-
-
-http.listen(port,()=>{
-  console.log("Listening on port ", port);
-});
