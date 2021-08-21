4ad4f647fd5aaf45b1ed4975cb0f9f3fb59529b4
Jordan Litsas <jordanlitsas@live.com.au>
2021-08-21 14:38:46 +1000

    Finished method convering table input into schema

--- public/js/env.js
+++ public/js/env.js
@@ -1,5 +1,5 @@
 import {getDBMSView} from '../resources/HTML_Views.js'
-import {tableCount, currentFocus, addTable, addAttribute, test} from './dbmsUserEntry.js'
+import {tableCount, currentFocus, addTable, addAttribute, test, tables} from './dbmsUserEntry.js'
 
 window.addEventListener('DOMContentLoaded', (event) => {
               
@@ -12,32 +12,83 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 //Global Variables
 var userInstance = {
-  user: {name: null, database: null},
-  database: null
+  username: null,
+  database: {
+      databaseName: null,
+      tables: []
+  }
+};
+
+const getTableObj = () =>{
+  return {
+    tableName: null,
+     rows: []
+    };
+}
+
+const getRowObj = () => {
+  return {
+    dataType: null,
+    attribute: null,
+    constraint: null
+  };
+}
+
+const saveDatabaseStateToUserInstance = () => {
+    
+  let currentRowNumber = 1;
+  let currentTableNumber = 0;
+
+ let tablesToConvert = tables.slice(0);
+
+ let currentViewedTable = document.getElementById('main').firstElementChild;
+ 
+  if (currentViewedTable != null){
+    tablesToConvert.push(currentViewedTable);
+  }
+  
+
+ 
+
+
+  //loop over each table
+  while (currentTableNumber < tableCount){
+
+    let table = getTableObj(); //base table json
+    table.tableName = document.getElementById(`tableNameInput${currentTableNumber}`).value; //get name from sidebar inputs
+    let rowCount = tablesToConvert[currentTableNumber].childNodes.length; //length = table rows = attributes to loop over (-1 because it includes labels)
+
+    //loop over each row of each table, skipping the first as this is the table header
+    while (currentRowNumber < rowCount){
+
+      let row = getRowObj(); //base row json
+      let rowElement = tablesToConvert[currentTableNumber].childNodes[currentRowNumber]; //capturingg each row element
+  
+      //capture each table data input's value, assign it to the row object
+      row.dataType = rowElement.childNodes[0].firstElementChild.value
+      row.attribute = rowElement.childNodes[1].firstElementChild.value
+      row.constraint = rowElement.childNodes[2].firstElementChild.value
+      
+      table.rows.push(row); //push unique row object into table object
+      currentRowNumber++; 
+    }
+
+    userInstance.database.tables.push(table); //push completed table object into collection of tables
+    currentTableNumber ++;
+    currentRowNumber = 1; //if you forget to reset nested loop's counter again, and spend an hour scratching your head like an idiot, you should go back to working in kitchens.
+    
+  }
+
 }
 
 
-
-
 const saveDatabase = () => {
-    let dataType = $('#data-type').val();
-    let attribute = $('#attribute').val();
-    let constraint = $('#constraint').val();
-    let tableName = $('#table1NameInput').val();
-    
-
-    
-    let table = {
-        'name': tableName,
-        'datatype': dataType,
-        'attribute': attribute,
-        'constraint': constraint
-    }
-    
+  saveDatabaseStateToUserInstance();
+  console.log(userInstance)
     $.ajax({
       url: '/saveDatabase',
       contentType: 'application/json',
-      data: JSON.stringify([userInstance.user, table]),
+      data: JSON.stringify(userInstance), //changed args
       type: 'POST',
       success: function(result){
         if (result.result == 200){
